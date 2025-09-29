fba70f2abf15018ab1ea98ca225e1d99e142b89b
Mario Reder <mario.reder@pm.me>
2025-09-29 06:57:59 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -26,7 +26,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
   "VITE_SEO_SITE_URL": "https://dex.orderly.network/baiyun-5014/",
-  "VITE_SEO_SITE_LANGUAGE": "en",
+  "VITE_SEO_SITE_LANGUAGE": "zh",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
   "VITE_SEO_THEME_COLOR": "#000000",
